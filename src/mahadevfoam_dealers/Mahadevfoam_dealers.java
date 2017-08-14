f55265b2c9a7c87d29dbb141eed9792e05455247
virajthakkar4699 <mahadevfoamviraj@gmail.com>
2017-08-14 19:42:49 +0530

    UPDATE

---
+++ src/mahadevfoam_dealers/Mahadevfoam_dealers.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahadevfoam_dealers;
+
+/**
+ *
+ * @author Mahadav
+ */
+public class Mahadevfoam_dealers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/mahadevfoam_dealers/rs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahadevfoam_dealers;
+
+/**
+ *
+ * @author Student
+ */
+class rs {
+
+    static boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getString(String ifnullmaxtaskid0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
